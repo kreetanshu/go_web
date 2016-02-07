0f27b8611a5d855bd801970f1661fc9a99f1defa
himanshu008 <sangalhimanshu08@gmail.com>
2016-02-07 22:14:04 +0530

    Last commit

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,8 +10,12 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+
 //= require jquery
+//= require jquery.ui.datepicker
 //= require jquery_ujs
+//= require bootstrap-datetimepicker
 //= require bootstrap
 
 //= require_tree .
+//= require_self
